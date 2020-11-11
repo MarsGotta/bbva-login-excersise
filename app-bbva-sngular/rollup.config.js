d486eca8571813f00e1f32930dff55812d412682
Marcela Gotta <marcelagotta@gmail.com>
2020-11-11 12:07:02 +0100

    feat: pwa manifest added

--- app-bbva-sngular/rollup.config.js
+++ app-bbva-sngular/rollup.config.js
@@ -1,6 +1,7 @@
 import merge from 'deepmerge';
 // use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
+import copy from 'rollup-plugin-copy';
 
 // use createBasicConfig to do regular JS to JS bundling
 // import { createBasicConfig } from '@open-wc/building-rollup';
@@ -17,13 +18,23 @@ const baseConfig = createSpaConfig({
   developmentMode: process.env.ROLLUP_WATCH === 'true',
 
   // set to true to inject the service worker registration into your index.html
-  injectServiceWorker: false,
+  injectServiceWorker: true,
 });
 
 export default merge(baseConfig, {
   // if you use createSpaConfig, you can use your index.html as entrypoint,
   // any <script type="module"> inside will be bundled by rollup
   input: './index.html',
+  plugins: [
+    copy({
+      targets: [
+        { src: './assets/**', dest: './dist' },
+        { src: './manifest.json', dest: './dist' },
+      ],
+      // set flatten to false to preserve folder structure
+      flatten: false,
+    }),
+  ],
 
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
